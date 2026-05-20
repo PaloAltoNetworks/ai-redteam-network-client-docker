849e5398bbc89da16a5bbafa5298614bca68faaf
lenoxys <3996456+lenoxys@users.noreply.github.com>
2026-05-20 17:23:44 +0200

    bump SCRIPT_VERSION to 0.1.1

--- setup-panw-network-client.sh
+++ setup-panw-network-client.sh
@@ -32,7 +32,7 @@ set -euo pipefail
 
 # --- Constants ---
 
-SCRIPT_VERSION="0.1.0"
+SCRIPT_VERSION="0.1.1"
 REGISTRY_DEFAULT="registry.ai-red-teaming.paloaltonetworks.com"
 REGISTRY="$REGISTRY_DEFAULT"
 KNOWN_REGISTRIES=(
